1c1bf2c3228e9582f1d18a777743b3ee5fa6ba08
sia <my@pers.on.al>
2022-12-18 08:27:18 -0500

    add contactPage

--- src/pages/client/Contact.jsx
+++ src/pages/client/Contact.jsx
@@ -1,20 +1,295 @@
-import React from "react";
+import React, { useState } from "react";
 import LayoutContent from "./shared/LayoutContent";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, Link } from "react-router-dom";
+import { Switch } from "@headlessui/react";
+import logo_couleur from "../../assets/logo/logo_carre.png";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Contact = () => {
   const location = useLocation();
   const params = useParams();
 
+  const [agreed, setAgreed] = useState(false);
+
   return (
     <LayoutContent>
       <div className="">
-        <div className="text-orange-500 text-6xl text-center font-thin ">
-          <span className="font-extralight italic text-9xl text-blue-500 ">
-            B
-          </span>
-          ientôt ...
-          <p>{location.pathname}</p>
+        <Link to={"/"}>
+          <img
+            className=" h-40 w-auto mx-auto"
+            src={logo_couleur}
+            alt="QTickets"
+          />
+        </Link>
+        <div className="overflow-hidden py-16 px-4 sm:px-6 lg:px-8 lg:py-12">
+          <div className="relative mx-auto max-w-xl">
+            <svg
+              className="absolute left-full translate-x-1/2 transform"
+              width={404}
+              height={404}
+              fill="none"
+              viewBox="0 0 404 404"
+              aria-hidden="true"
+            >
+              <defs>
+                <pattern
+                  id="85737c0e-0916-41d7-917f-596dc7edfa27"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={404}
+                fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+              />
+            </svg>
+            <svg
+              className="absolute right-full bottom-0 -translate-x-1/2 transform"
+              width={404}
+              height={404}
+              fill="none"
+              viewBox="0 0 404 404"
+              aria-hidden="true"
+            >
+              <defs>
+                <pattern
+                  id="85737c0e-0916-41d7-917f-596dc7edfa27"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={404}
+                fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+              />
+            </svg>
+            <div className="text-center">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Formulaire de contact
+              </h2>
+              <p className="mt-4 text-lg leading-6 text-gray-500 text-justify ">
+                Remplissez le formulaire pour en connaître plus sur votre
+                demande ... L'équipe vous répondra dans les plus bref délai
+              </p>
+            </div>
+            <div className="mt-12">
+              <form
+                action="#"
+                method="POST"
+                className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+              >
+                <div>
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    First name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Last name
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="company"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Company
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="company"
+                      id="company"
+                      autoComplete="organization"
+                      className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="phone-number"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Phone Number
+                  </label>
+                  <div className="relative mt-1 rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 flex items-center">
+                      <label htmlFor="country" className="sr-only">
+                        Country
+                      </label>
+                      <select
+                        id="country"
+                        name="country"
+                        className="h-full rounded-md border-transparent bg-transparent py-0 pl-4 pr-8 text-gray-500 focus:border-blue-500 focus:ring-blue-500"
+                      >
+                        <option>FR</option>
+                        <option>US</option>
+                        <option>CA</option>
+                        <option>EU</option>
+                      </select>
+                    </div>
+                    <input
+                      type="text"
+                      name="phone-number"
+                      id="phone-number"
+                      autoComplete="tel"
+                      className="block w-full rounded-md border-gray-300 py-3 px-4 pl-24 focus:border-blue-500 focus:ring-blue-500"
+                      placeholder="+1 (555) 987-6543"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="phone-number"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Select a type of Message
+                  </label>
+                  <div className=" mt-1 rounded-md shadow-sm ">
+                    <div className="flex items-center ">
+                      <select
+                        id="type"
+                        name="type"
+                        className="h-full rounded-md border-transparent bg-transparent py-2 pl-4 pr-8 text-gray-500 bg-white focus:border-blue-500 focus:ring-blue-500 "
+                      >
+                        <option>Devenir Céateur d'évenement </option>
+                        <option>J'ai un problème technique</option>
+                        <option>Je souhaite un partenariat</option>
+                        <option>Autres </option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Message
+                  </label>
+                  <div className="mt-1">
+                    <textarea
+                      id="message"
+                      name="message"
+                      rows={4}
+                      className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <div className="flex items-start">
+                    <div className="flex-shrink-0">
+                      <Switch
+                        checked={agreed}
+                        onChange={setAgreed}
+                        className={classNames(
+                          agreed ? "bg-blue-600" : "bg-gray-200",
+                          "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                        )}
+                      >
+                        <span className="sr-only">Agree to policies</span>
+                        <span
+                          aria-hidden="true"
+                          className={classNames(
+                            agreed ? "translate-x-5" : "translate-x-0",
+                            "inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+                          )}
+                        />
+                      </Switch>
+                    </div>
+                    <div className="ml-3">
+                      <p
+                        className={`text-base ${
+                          !agreed ? "text-red-500" : "text-blue-500"
+                        }`}
+                      >
+                        Je ne suis pas un robot !
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <button
+                    type="submit"
+                    className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                  >
+                    Let's talk
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </LayoutContent>
--- src/pages/client/shared/LayoutContent.jsx
+++ src/pages/client/shared/LayoutContent.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const LayoutContent = ({ children }) => {
-  return (
-    <div className=" min-h-screen p-5 lg:p-10 bg-slate-200 ">{children}</div>
-  );
+  return <div className=" min-h-screen   bg-slate-100 ">{children}</div>;
 };
 
 export default LayoutContent;
